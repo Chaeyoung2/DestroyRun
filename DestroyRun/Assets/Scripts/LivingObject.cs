f901e7ffc43d61f0c2f8afbe484b5c5e11f5da4e
Chaeyoung2 <xxll0331@hanmail.net>
2020-06-25 23:02:08 +0900

    add LivingObject class
    모든 생명 클래스가 부모로 가짐

---
+++ DestroyRun/Assets/Scripts/LivingObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 생명체로서 동작할 게임 오브젝트들을 위한 뼈대를 제공
+// 체력, 데미지 받아들이기, 사망 기능, 사망 이벤트를 제공
+
+enum enemy_state { state_idle, state_run, state_damaged, state_attack, state_dead }
+
+
+public class LivingObject : MonoBehaviour
+{
+    private void Start()
+    {
+      
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        
+    }
+}
