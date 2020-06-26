43d68140e8ffdcd83d2233de9c2f4d592cfe9cf8
Chaeyoung2 <xxll0331@hanmail.net>
2020-06-26 23:35:58 +0900

    add GameController
    게임 메인 씬 관리

---
+++ DestroyRun/Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Gamecontroller: MonoBehaviour
+{
+    public GameObject playerPrefab;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
