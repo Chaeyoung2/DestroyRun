45f3cc646cf8f87556bbaa40842e003b233faef9
Chaeyoung2 <xxll0331@hanmail.net>
2020-07-02 22:03:17 +0900

    add UI - Input,Image,Text Component

---
+++ DestroyRun/Assets/Scripts/Menu/EventSubscriber.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventSubscriber : MonoBehaviour
+{
+    [SerializeField] private Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        // 클릭되었을 때 유니티 메서드 호출
+        button.onClick.AddListener( ()=> { // lambda
+            // loadScene();
+        }); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
