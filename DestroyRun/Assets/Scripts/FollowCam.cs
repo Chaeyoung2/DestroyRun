a796d7bcea28f05c530d7993341fa6eb7c10fcb6
Chaeyoung2 <xxll0331@hanmail.net>
2020-07-03 02:02:50 +0900

    add Player's Auto Moving, Follow cam

---
+++ DestroyRun/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public Transform target;
+    private Transform tr;
+
+    public float CameraZ = -5;
+    public float CameraSpeed;
+
+    private Vector3 velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 targetPos = target.position;
+        targetPos.y += 15;
+        tr.position = Vector3.Lerp(tr.position, targetPos, Time.deltaTime * CameraSpeed);
+    }
+}
