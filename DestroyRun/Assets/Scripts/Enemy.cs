0dc2e496f9b0a798b00963369592737d22d428e6
Chaeyoung2 <xxll0331@hanmail.net>
2020-07-03 05:54:07 +0900

    add Enemy script - targeting, initialize, die

---
+++ DestroyRun/Assets/Scripts/Enemy.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI; // AI, 내비게이션 시스템 관련 코드를 가져오기
+
+// 적 AI를 구현한다
+public class Enemy : LivingObject
+{
+
+    private LivingObject targetEntity; // 추적할 대상
+
+    public ParticleSystem hitEffect; // 피격시 재생할 파티클 효과
+
+    private Animator enemyAnimator; // 애니메이터 컴포넌트
+    private Renderer enemyRenderer; // 렌더러 컴포넌트
+
+    public float damage = 20f; // 공격력
+    public float timeBetAttack = 0.5f; // 공격 간격
+    private float lastAttackTime; // 마지막 공격 시점
+
+
+    // 추적할 대상이 존재하는지 알려주는 프로퍼티
+    private bool hasTarget
+    {
+        get
+        {
+            // 추적할 대상이 존재하고, 대상이 사망하지 않았다면 true
+            if (targetEntity != null && !targetEntity.dead)
+            {
+                return true;
+            }
+
+            // 그렇지 않다면 false
+            return false;
+        }
+    }
+
+    private void Awake()
+    {
+        // 게임 오브젝트로부터 사용할 컴포넌트들을 가져오기
+        enemyAnimator = GetComponent<Animator>();
+
+        // 렌더러 컴포넌트는 자식 게임 오브젝트에게 있으므로
+        // GetComponentInChildren() 메서드를 사용
+        enemyRenderer = GetComponentInChildren<Renderer>();
+    }
+
+    // 적 AI의 초기 스펙을 결정하는 셋업 메서드
+   public void Setup(float newHealth, float newDamage, float newSpeed, Color skinColor)
+    {
+        // 체력 설정
+        startingHealth = newHealth;
+        health = newHealth;
+        // 공격력 설정
+        damage = newDamage;
+        // 렌더러가 사용중인 머테리얼의 컬러를 변경, 외형 색이 변함
+        enemyRenderer.material.color = skinColor;
+    }
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        // 추적 대상의 존재 여부에 따라 다른 애니메이션을 재생
+        enemyAnimator.SetBool("HasTarget", hasTarget);
+    }
+
+    // 데미지를 입었을때 실행할 처리
+    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal)
+    {
+    }
+
+    // 사망 처리
+    public override void Die()
+    {
+        // LivingEntity의 Die()를 실행하여 기본 사망 처리 실행
+        base.Die();
+
+        // 다른 AI들을 방해하지 않도록 자신의 모든 콜라이더들을 비활성화
+        Collider[] enemyColliders = GetComponents<Collider>();
+        for (int i = 0; i < enemyColliders.Length; i++)
+        {
+            enemyColliders[i].enabled = false;
+        }
+    }
+
+}
