48fd4c95a77186cf5b7c43f0da70daafc62e391b
Chaeyoung2 <xxll0331@hanmail.net>
2020-07-08 15:32:10 +0900

    player 기본 이동

---
+++ DestroyRun/Assets/Scripts/TestCharacterController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCharacterController : MonoBehaviour
+{
+    public float movementSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hMovement = Input.GetAxis("Horizontal") * movementSpeed;
+        float vMovement = Input.GetAxis("Vertical") * movementSpeed / 2;
+
+        transform.Translate(new Vector3(hMovement, 0, vMovement) * Time.deltaTime);
+    }
+}
