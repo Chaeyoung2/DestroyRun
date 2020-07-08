aabf67974a70e98b78d19db260101d5bdcfdffbc
Chaeyoung2 <xxll0331@hanmail.net>
2020-07-08 18:49:05 +0900

    맵 터레인, 오브젝트 배치

---
+++ DestroyRun/Assets/Scripts/PlotSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlotSpawner : MonoBehaviour
+{
+    private int inotAmount = 5;
+    private float intStartZ = 5;
+    private float plotSize = 60f;
+    private float xPosLeft;
+    private float xPosRight;
+    private float lastZPos;
+
+    public List<GameObject> plots;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
